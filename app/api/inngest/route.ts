d7a892bf72f02831fcbb3b29d8b00f49f493f5e2
Amna Sehgal <amnasehgal211@gmail.com>
2026-06-01 21:48:03 +0530

    inngest

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,8 +1,7 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/inngest";
-import { helloWorld, sendInterviewReminder } from "@/lib/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [helloWorld, sendInterviewReminder],
+  functions: [],
 });
